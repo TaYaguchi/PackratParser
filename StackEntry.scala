5798139481f287274f0cf9f942abeabc35cbe314
TaYaguchi <ryokutyou@gmail.com>
2014-12-12 22:09:35 +0900

    Revert "PackratParserGenerator"
    This reverts commit 151dc7d85f25d55996cc07701b9be731697e480f.

--- StackEntry.scala
+++
@@ -1,5 +0,0 @@
-abstract class StackEntry
-	case class BackEntry(pc : Int, pos : Int, sp : Int) extends StackEntry
-	case class ReturnEntry(pc : Int, sp : Int, ip : Int, nt : Integer) extends StackEntry
-	//case class SemanticEntry(sv : Any) extends StackEntry
-	case class StackPointerEntry(sp : Int, ip : Int) extends StackEntry
